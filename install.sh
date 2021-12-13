b9f3489b9105cfb20c758d367cae092fdcdb59e7
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2021-12-12 20:02:07 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,7 @@
 
+
+## Consumer Test
+bin/pulsar-client consume "persistent://public/default/chatresult2" -s "chatresult2" -n 2
+
+## Reference
+echo 'https://github.com/tspannhw/pulsar-pychat-function'
