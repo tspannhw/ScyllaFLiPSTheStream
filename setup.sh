4cefa68d3d61d8754ec0a1df0e7a3fcd1cb4b925
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2021-12-14 13:12:16 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,3 +2,8 @@ bin/pulsar-admin topics create persistent://public/default/test_scylla
 
 bin/pulsar-admin sinks create --tenant public --namespace default --name "sink" \
     --sink-type cassandra --sink-config-file conf/scylla.yml --inputs test_scylla
+    
+bin/pulsar-admin topics create persistent://public/default/test_scylla
+
+bin/pulsar-admin sinks create --tenant public --namespace default --name "sink" \
+    --sink-type cassandra --sink-config-file conf/scylla.yml --inputs test_scylla
