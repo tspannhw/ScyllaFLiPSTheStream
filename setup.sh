d62b16cf9eb55f689a49fcfecd4db39fc2ea697d
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2021-12-14 13:21:33 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,3 +7,10 @@ bin/pulsar-admin topics create persistent://public/default/test_scylla
 
 bin/pulsar-admin sinks create --tenant public --namespace default --name "sink" \
     --sink-type cassandra --sink-config-file conf/scylla.yml --inputs test_scylla
+
+
+bin/pulsar-admin topics create persistent://public/default/iotjetsonjson
+bin/pulsar-client consume "persistent://public/default/iotjetsonjson" -s "iotjjreader" -n 0
+
+bin/pulsar-admin topics create persistent://public/default/energy
+bin/pulsar-client consume "persistent://public/default/energy" -s "mobile2" -n 0
